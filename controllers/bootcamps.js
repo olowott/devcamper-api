046e70d4f2c0722cece8d21c0838292ae9d32c22
Tope Olowookere <olowott@Topes-MacBook-Pro.local>
2020-04-04 10:41:40 +0100

    selecting and sorting with mongoose

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,11 +9,40 @@ const Bootcamp = require('../models/Bootcamp'); // models
 //@access   Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
-  let queryStr = JSON.stringify(req.query);
 
+  // copy req.query
+  const reqQuery = { ...req.query };
+
+  //Fields to exclude from query search or selection
+  const removeFields = ['select', 'sort'];
+
+  //loop over removeFields and delete them fro reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  //create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  //create opearator ($gt, $gte, etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  //Finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  //Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  //Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createAt');
+  }
+
+  //Executing our query
   const bootcamps = await query;
   res
     .status(200)
