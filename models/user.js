e986061e1fb93f7bc0744562ec053820a6bb0d9b
Tope Olowookere <olowott@gmail.com>
2020-04-05 11:43:20 +0100

    adding jwtwebtoken

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -34,10 +35,17 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-//Encrypt password using bcrypt
+//Encrypt password using bcrypt middleware
 UserSchema.pre('save', async function(next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+//Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
 
 module.exports = mongoose.model('user', UserSchema);
