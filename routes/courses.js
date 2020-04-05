1a3088c0cfbf6db4764a95da179d2b40be672a73
Tope Olowookere <olowott@gmail.com>
2020-04-05 13:58:06 +0100

    adding roles with access

--- routes/courses.js
+++ routes/courses.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const {
   getCourses,
   getCourse,
@@ -11,6 +12,7 @@ const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
+const { protect, authorize } = require('../middleware/auth');
 
 router
   .route('/')
@@ -21,11 +23,11 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
 router
   .route('/:id')
   .get(getCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
