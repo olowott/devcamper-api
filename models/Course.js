ecf4d6c7355ac6d3e2147b804e42626f548a610b
Tope Olowookere <olowott@gmail.com>
2020-04-06 08:26:08 +0100

    CRUD realtionship btw user bootcamp and courses

--- models/Course.js
+++ models/Course.js
@@ -4,56 +4,61 @@ const CourseSchema = new mongoose.Schema({
   title: {
     type: String,
     trim: true,
-    require: [true, 'Please add a course title']
+    require: [true, 'Please add a course title'],
   },
   description: {
     type: String,
-    required: [true, 'Please add a descritpion']
+    required: [true, 'Please add a descritpion'],
   },
   weeks: {
     type: String,
-    required: [true, 'Please add number of weeks']
+    required: [true, 'Please add number of weeks'],
   },
   tuition: {
     type: Number,
-    required: [true, 'Please add a tuition cost']
+    required: [true, 'Please add a tuition cost'],
   },
   minimumSkill: {
     type: String,
     required: [true, 'Please add a minimum skill'],
-    enum: ['beginner', 'intermediate', 'advanced']
+    enum: ['beginner', 'intermediate', 'advanced'],
   },
   scholarshipAvailable: {
     type: Boolean,
-    default: false
+    default: false,
   },
   CreatedAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   bootcamp: {
     type: mongoose.Schema.ObjectId,
     ref: 'Bootcamp',
-    required: true
-  }
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 });
 
 //static method to get avg of course tuitions
-CourseSchema.statics.getAverageCost = async function(bootcampId) {
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
   const obj = await this.aggregate([
     {
-      $match: { bootcamp: bootcampId }
+      $match: { bootcamp: bootcampId },
     },
     {
       $group: {
         _id: '$bootcamp',
-        averageCost: { $avg: '$tuition' }
-      }
-    }
+        averageCost: { $avg: '$tuition' },
+      },
+    },
   ]);
   try {
     await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
-      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
     });
   } catch (err) {
     console.log(err);
@@ -61,12 +66,12 @@ CourseSchema.statics.getAverageCost = async function(bootcampId) {
 };
 
 //middleware to call getAverageCost after save
-CourseSchema.post('save', function() {
+CourseSchema.post('save', function () {
   this.constructor.getAverageCost(this.bootcamp);
 });
 
 //middleware to call getAverageCost before remove
-CourseSchema.pre('save', function() {
+CourseSchema.pre('save', function () {
   this.constructor.getAverageCost(this.bootcamp);
 });
 
