64aa1cf6c0527993f0ff1599e797b6adc3c0a235
Tope Olowookere <olowott@gmail.com>
2020-04-07 13:36:50 +0100

    logout user fully and remove token cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -63,6 +63,21 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
+//@desc     log use out/ clear cookie
+//@route    GET /api/v1/auth/logout
+//@access   Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 //@desc     Update logged in user details
 //@route    PUT /api/v1/auth/updatedetails
 //@access   Private
--- middleware/auth.js
+++ middleware/auth.js
@@ -11,9 +11,10 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    //set toekn from bearer token in header
     token = req.headers.authorization.split(' ')[1];
   }
-
+  // set toekn from cookie
   //   else if(req.cookies.token){
   //     token = req.cookies.token
   //   }
